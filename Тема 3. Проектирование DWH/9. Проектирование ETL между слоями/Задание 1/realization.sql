12b8dedaac22c550ad5894debe3e6e06eabdd85d
Saxar1 <saxarr0k1@gmail.com>
2023-04-14 22:29:37 +0300

    update_14-04-23

--- Тема 3. Проектирование DWH/9. Проектирование ETL между слоями/Задание 1/realization.sql
+++ Тема 3. Проектирование DWH/9. Проектирование ETL между слоями/Задание 1/realization.sql
@@ -8,11 +8,48 @@
 -- orders_bonus_granted_sum
 -- order_processing_fee
 
+with 
+restaurant_m as (
+    select *
+    from dds.dm_restaurants),
+order_m as (
+    select
+        id,
+    	restaurant_id,
+    	timestamp_id
+    from dds.dm_orders
+    where order_status = 'CLOSED'),
+date_m as (
+	select
+		id,
+		"date"
+	from dds.dm_timestamps),
+fps_m as (
+    select
+        order_id,
+        total_sum,
+        bonus_payment,
+        bonus_grant
+    from dds.fct_product_sales)
+select 
+	rm.id,
+	rm.restaurant_name,
+	dm.date as settlement_date,
+	count(om.id) as orders_count,
+	sum(fm.total_sum) as order_total_sum,
+	sum(fm.bonus_payment) as order_bonus_payment_sum,
+	sum(fm.bonus_grant) as order_bonus_granted_sum,
+	sum(fm.total_sum) * 0.25 as order_processing_fee,
+	(sum(fm.total_sum) - sum(fm.total_sum) * 0.25 - sum(fm.bonus_payment)) as restaurant_reward_sum
+from restaurant_m as rm
+join order_m as om on om.restaurant_id = rm.id
+join date_m as dm on dm.id = om.timestamp_id
+join fps_m as fm on om.id = fm.order_id
+group by rm.id, rm.restaurant_name, dm.date;
 
-select
-    fps.count as orders_count,
-    fps.total_sum as orders_total_sum,
-    fps.bonus_payment as orders_bonus_payment_sum,
-    fps.bonus_granted as orders_bonus_granted_sum,
-    order_processing_fee
-from dds.fct_product_sales as fps`
+
+
+
+
+
+
